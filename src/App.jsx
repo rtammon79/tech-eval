e4e7c084f79f749635d8ffa1ee85bccc2d12bb89
rtammon79 <52932414+rtammon79@users.noreply.github.com>
2024-05-23 08:54:56 -0400

    add some comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
+//import routing tools needed to create and use react-router-dom to bring in additional
+//pages
 import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
 } from 'react-router-dom';
+//import for setting up main layout(parent route element) for anything
+//that should be on all pages, such as the Navbar
 import MainLayout from './layouts/MainLayout';
+//import homepage to be the index element home/start page at / (ex. localhost:3000/)
 import HomePage from './pages/HomePage';
 
-// import Navbar from './components/Navbar';
-// import Hero from './components/Hero';
-// import HomeCards from './components/HomeCards';
-// import JobListings from './components/JobListings';
-// import ViewAllJobs from './components/ViewAllJobs';
-
+//creation of browser router. creating parent route at '/' to hold navbar and anything
+//that needs to be on all pages, and all the children routes that need to be navigated to
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
@@ -23,16 +24,6 @@ const router = createBrowserRouter(
 
 const App = () => {
   return <RouterProvider router={router} />;
-
-  // return (
-  //   <>
-  //     <Navbar />
-  //     <Hero />
-  //     <HomeCards />
-  //     <JobListings />
-  //     <ViewAllJobs />
-  //   </>
-  // );
 };
 
 export default App;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,7 @@
+//import outlet to render all children route's elements
+//that get applied in the main (App.jsx)
 import { Outlet } from 'react-router-dom';
+//import navbar here so that it will be in the Parent route and displayed on all pages
 import Navbar from '../components/Navbar';
 
 const MainLayout = () => {
