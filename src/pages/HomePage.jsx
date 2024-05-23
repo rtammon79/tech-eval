dad4185725b5a84c3fff3ee55678209b7d88802d
rtammon79 <52932414+rtammon79@users.noreply.github.com>
2024-05-23 08:40:11 -0400

    add components for homepage navbar viewalljobs hero card joblistings and joblisting

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import Hero from '../components/Hero';
+import HomeCards from '../components/HomeCards';
+import Joblistings from '../components/JobListings';
+import ViewAllJobs from '../components/ViewAllJobs';
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <HomeCards />
+      <Joblistings />
+      <ViewAllJobs />
+    </>
+  );
+};
+export default HomePage;
